8fcd06505fd4394056c03eecb6b2e862379efd36
Brownster <marcsmobile.brown@googlemail.com>
2014-11-04 17:46:58 +0000

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,61 +1,62 @@
 #!/bin/bash
 
+DYNDNS=someplace.dydns-remote.com
 echo "the current setting for your DNS Host name for your VPS is Currently $DYNDNS"
 #DYNDNS / noip host name that resolves into your vps ip address
-DYNDNS=someplace.dydns-remote.com
 
+WEBUSER=webuser
 echo "Your current username for all web apps is $WEBUSER"
 #Please enter a user name for accessing sickbeard, couchpotato ect
-WEBUSER=webuser
 
+WEBPASS=webpass
 echo "Your current web app password id $WEBPASS"
 #Please enter a password for accessing all the web apps sickbeard, couchpotato ect
-WEBPASS=webpass
 
+SQUIDUSER=squid
 echo "Squid Proxy User name is $SQUIDUSER"
 #Please enter a Username for Squid Proxy Server
-SQUIDUSER=squid
 
+SQUIDPASS=hideme
 echo "Your Squid Proxy password is $SQUIDPASS"
 #Please enter a password for Squid Proxy Server
-SQUIDPASS=hideme
 
+SQUIDPORT=7629
 echo "The IP Port being used by Squid Proxy is $SQUIDPORT"
 #squid Proxy please enter the port for web access
-SQUIDPORT=7629
 
+SSHPORT=2022
 echo "the port remote SSH sessions will be set to is $SSHPORT"
 #SSH please enter the port for access
-SSHPORT=2022
 
+FTPHOST=somewhere.dyndns-remote.com
 echo "Your DYNDNS host name for your home is set to $FTPHOST"
 #FTP server address eith ip address if you have static address or 
 #dyn dns / no ip account resolving to your home ip if you are dynamic
-FTPHOST=somewhere.dyndns-remote.com
 
+FTPUSER=ftpuser
 echo "Your local ftp server username is $FTPUSER"
 #ftp user
-FTPUSER=ftpuser
 
+FTPPASS=ftppass
 echo "Your local FTP server password is $FTPPASS"
 #ftp password
-FTPPASS=ftppass
 
+FILMFTPDIR=films
 echo "Your Local FTP directory for Films relative to ftp home directory is $FILMFTPDIR"
 #film ftp location - relative to ftp home directory
-FILMFTPDIR=films
 
+TVFTPDIR=tvseries
 echo "Your Local FTP directory for TV relative to ftp home directory is $TVFTPDIR"
 #TV ftp location
-TVFTPDIR=tvseries
 
+MUSICFTPDIR=music
 echo "Your Local FTP directory for Music relative to ftp home directory is $MUSICFTPDIR"
 #Music ftp location
-MUSICFTPDIR=music
 
+BOOKSFTPDIR=ebooks
 echo "Your Local FTP directory for Books relative to ftp home directory is $BOOKSFTPDIR"
 #Books ftp location
-BOOKSFTPDIR=ebooks
+
 
 #Games ftp location
 GAMESFTPDIR=games
@@ -69,17 +70,18 @@ GAMESMNTDIR=/home/media/games
 #comics mount location
 COMICSMNTDIR=/home/media/comics
 
+FILMMNTDIR=/home/media/films
 echo "Your VPS film dir mount point is $FILMMNTDIR"
 #films mount location
-FILMMNTDIR=/home/media/films
 
+TVMNTDIR=/home/media/tv
 echo "Your VPS TV dir mount point is $TVMNTDIR"
 #tv series mount location
-TVMNTDIR=/home/media/tv
 
+MUSICMNTDIR=/home/media/music
 echo "your VPS Music dir mount point is $MUSICMNTDIR"
 #music mount location
-MUSICMNTDIR=/home/media/music
+
 
 #books mount location
 BOOKSMNTDIR=/home/media/books
@@ -89,12 +91,17 @@ BOOKSMNTDIR=/home/media/books
 
 #SABNZB Please enter the port for web access
 SABPORT=7960
+echo "the port used by SABNZB will be $SABPORT"
+
+
 
 #SICKBEARD Please enter the port for web access
 SICKPORT=7961
+echo "the port used by Sickbeard will be $SICKPORT"
 
 #COUCHPOTATO Please enter the port for web access
 COUCHPORT=7962
+echo "the port used by Couchpotato will be $COUCHPORT"
 
 #Headphones Please enter the port for web access
 HEADPORT=7963
@@ -117,6 +124,10 @@ TRANPPORT=61724
 #Maraschino Web UI port
 MARAPORT=7979
 
+#SET WAN Address
+HOSTIP=`ifconfig|xargs|awk '{print $7}'|sed -e 's/[a-z]*:/''/'`
+echo "You will be using: $HOSTIP as the WAN address of your VPS"
+
 
 echo "if any of the above information is incorrect please quit this scrpit open it in vi and edit the first section to your requirements"
 clear
@@ -132,12 +143,16 @@ do
     ------------------------------
     Please enter your choice:
 
-    Harden VPS          (1)
-    Install SabNZB      (2)
-    Install SickBeard   (3)
-    Install Couchpotato (4)
-    Install Headphones	(5)
-    Install NZB indexer (6)
+    Harden VPS (see readme)     (1)
+    Install SabNZB      	(2)
+    Install SickBeard   	(3)
+    Install Couchpotato 	(4)
+    Install Headphones		(5)
+    Install Lazy Librarian	(6)
+    Install Squid Proxy 	(7)
+    Install Transmission	(8)
+    Install nZEDb indexer	(9)
+    Install 1GB Swap space	(M)
            (Q)uit
     ------------------------------
 EOF
@@ -146,8 +161,7 @@ EOF
     "1")  add-apt-repository ppa:jcfp/ppa
 add-apt-repository ppa:transmissionbt/ppa
 apt-get update
-HOSTIP=`ifconfig|xargs|awk '{print $7}'|sed -e 's/[a-z]*:/''/'`
-echo "i will be using: $HOSTIP as the WAN address"
+
 
 echo "#######################"
 echo "## create a new user ##"
@@ -601,6 +615,469 @@ chmod 777 /home/$username/.couchpotato/
 echo "starting couchpotato"
 python /home/$username/.couchpotato/CouchPotato.py --daemon
 echo "CouchPotato has been started on port $COUCHPORT" ;;
+
+
+    "5") echo "###########################"
+echo "## installing Headphones ##"
+echo "###########################"
+sleep 2
+cd /home/$username/temp
+git clone https://github.com/rembo10/headphones.git  headphones
+cp /home/$username/temp/headphones /home/backups/headphones/
+mv /home/$username/temp/headphones /home/$username/.headphones
+sudo cp /home/$username/.headphones/init.ubuntu /etc/init.d/headphones
+mv /home/$username/.headphones/config.ini /home/$username/.headphones/config.orig
+touch /home/$username/.headphones/config.ini
+chown $username /home/$username/.headphones/*
+chown $username /home/$username/.headphones/*/*
+echo "will try and move config probably wont be there"
+mv /home/$username/.headphones/config.ini /home/$username/.headphones/config.old
+cat > /home/$username/.headphones/config.ini << EOF
+[General]
+config_version = 5
+http_port = $HEADPORT
+http_host = $HOSTIP
+http_username = $WEBUSER
+http_password = $WEBPASS
+http_root = /
+http_proxy = 0
+enable_https = 0
+https_cert = /home/$username/.headphones/server.crt
+https_key = /home/$username/.headphones/server.key
+launch_browser = 1
+api_enabled = 0
+api_key = ""
+log_dir = /home/$username/.headphones/logs
+cache_dir = /home/$username/.headphones/cache
+git_path = ""
+git_user = rembo10
+git_branch = master
+check_github = 1
+check_github_on_startup = 1
+check_github_interval = 360
+music_dir = /home/music
+destination_dir = /home/music
+lossless_destination_dir = ""
+preferred_quality = 0
+preferred_bitrate = ""
+preferred_bitrate_high_buffer = ""
+preferred_bitrate_low_buffer = ""
+preferred_bitrate_allow_lossless = 0
+detect_bitrate = 0
+auto_add_artists = 1
+correct_metadata = 1
+move_files = 1
+rename_files = 1
+folder_format = $Artist/$Album [$Year]
+file_format = $Track $Artist - $Album ($Year) - $Title
+file_underscores = 0
+cleanup_files = 1
+add_album_art = 1
+album_art_format = folder
+embed_album_art = 1
+embed_lyrics = 0
+nzb_downloader = 0
+torrent_downloader = 1
+download_dir = /home/completed/music
+blackhole_dir = ""
+usenet_retention = 1200
+include_extras = 0
+extras = ""
+autowant_upcoming = 1
+autowant_all = 0
+keep_torrent_files = 0
+numberofseeders = 10
+torrentblackhole_dir = /home/torrents
+isohunt = 0
+kat = 1
+mininova = 0
+piratebay = 1
+piratebay_proxy_url = ""
+download_torrent_dir = /home/completed/music
+search_interval = 360
+libraryscan = 1
+libraryscan_interval = 1800
+download_scan_interval = 5
+update_db_interval = 24
+mb_ignore_age = 365
+preferred_words = ""
+ignored_words = ""
+required_words = ""
+lastfm_username = ""
+interface = default
+folder_permissions = 0755
+file_permissions = 0644
+music_encoder = 0
+encoder = ffmpeg
+xldprofile = ""
+bitrate = 192
+samplingfrequency = 44100
+encoder_path = ""
+advancedencoder = ""
+encoderoutputformat = mp3
+encoderquality = 2
+encodervbrcbr = cbr
+encoderlossless = 1
+delete_lossless_files = 1
+mirror = headphones
+customhost = localhost
+customport = 5000
+customsleep = 1
+hpuser = 
+hppass = 
+[Waffles]
+waffles = 0
+waffles_uid = ""
+waffles_passkey = ""
+[Rutracker]
+rutracker = 0
+rutracker_user = ""
+rutracker_password = ""
+[What.cd]
+whatcd = 0
+whatcd_username = ""
+whatcd_password = ""
+[SABnzbd]
+sab_host = http://$HOSTIP:$SABPORT/sabnzbd
+sab_username = $WEBUSER
+sab_password = $WEBPASS
+sab_apikey = 
+sab_category = Music
+[NZBget]
+nzbget_username = nzbget
+nzbget_password = ""
+nzbget_category = ""
+nzbget_host = ""
+[Headphones]
+headphones_indexer = 1
+[Transmission]
+transmission_host = http://$HOSTIP:$TRANPORT
+transmission_username = $WEBUSER
+transmission_password = $WEBPASS
+[uTorrent]
+utorrent_host = ""
+utorrent_username = ""
+utorrent_password = ""
+[Newznab]
+newznab = 1
+newznab_host = http://
+newznab_apikey = 
+newznab_enabled = 1
+extra_newznabs = 
+[NZBsorg]
+nzbsorg = 0
+nzbsorg_uid = None
+nzbsorg_hash = ""
+[NZBsRus]
+nzbsrus = 0
+nzbsrus_uid = ""
+nzbsrus_apikey = ""
+[omgwtfnzbs]
+omgwtfnzbs = 0
+omgwtfnzbs_uid = ""
+omgwtfnzbs_apikey = ""
+[Prowl]
+prowl_enabled = 0
+prowl_keys = ""
+prowl_onsnatch = 0
+prowl_priority = 0
+[XBMC]
+xbmc_enabled = 0
+xbmc_host = ""
+xbmc_username = ""
+xbmc_password = ""
+xbmc_update = 0
+xbmc_notify = 0
+[NMA]
+nma_enabled = 0
+nma_apikey = ""
+nma_priority = 0
+nma_onsnatch = 0
+[Pushover]
+pushover_enabled = 0
+pushover_keys = ""
+pushover_onsnatch = 0
+pushover_priority = 0
+[Synoindex]
+synoindex_enabled = 0
+[Advanced]
+album_completion_pct = 80
+cache_sizemb = 32
+journal_mode = wal
+EOF
+
+
+cp /home/$username/.headphones/config.ini /etc/default/headphones
+chown $username /home/$username/.headphones/
+chown $username /home/$username/.headphones/*
+chown $username /home/$username/.headphones/*/*
+chmod 777 /home/$username/.headphones/*
+chmod 777 /home/$username/.headphones/logs/headphones.log
+chown $username /home/$userna
+chmod +x /etc/init.d/headphones  
+update-rc.d headphones defaults
+echo "starting Headphones on port $HEADPORT"   
+python /home/$username/.headphones/Headphones.py --daemon
+echo "Headphones has started you can try http://$HOSTIP:$HEADPORT"
+sleep 3;;
+
+
+    "6") echo "##############################"
+echo "## installing Lazylibrarian ##"
+echo "##############################"
+cd /home/$username/temp
+git clone https://github.com/Conjuro/LazyLibrarian.git lazylibrarian 
+cp /home/$username/temp/lazylibrarian /home/backups/lazylibrarian/
+mv /home/$username/temp/lazylibrarian  /home/$username/.lazylibrarian 
+cp /home/$username/.lazylibrarian/init/ubuntu.initd /etc/init.d/lazylibrarian
+
+cat > /etc/default/lazylibrarian << EOF
+APP_PATH=/home/castro/.lazylibrarian
+ENABLE_DAEMON=1
+RUN_AS=$user
+WEBUPDATE=0
+CONFIG=/home/$username/.lazylibrarian/
+DATADIR=/home/$username/.lazylibrarian/
+PORT=$BOOKPORT
+PID_FILE=/home/$username/.pid/lazylibrarian.pid
+EOF
+
+cat < /home/$username/.pid/lazylibrarian.pid << EOF
+50005
+EOF
+
+chown $username /home/$username/.pid/lazylibrarian.pid
+chmod 777 /home/$username/.pid/lazylibrarian.pid
+chown $username /home/$username/.lazylibrarian
+chmod 777 /home/$username/.lazylibrarian
+chmod +x /etc/init.d/lazylibrarian  
+update-rc.d lazylibrarian  defaults
+echo "Lazy Librarian will start on nect boot you can access the ui via http://$HOSTIP:$BOOKPORT"
+sleep 3;;
+
+    "7") echo "###################################"
+echo "## installing squid proxy server ##"
+echo "###################################"
+sleep 2
+sudo apt-get install squid3 squid3-common -y
+
+cat > /etc/squid3/squid.conf << EOF
+http_port $SQUIDPORT
+via off
+forwarded_for off
+request_header_access Allow allow all
+request_header_access Authorization allow all 
+request_header_access WWW-Authenticate allow all 
+request_header_access Proxy-Authorization allow all 
+request_header_access Proxy-Authenticate allow all 
+request_header_access Cache-Control allow all 
+request_header_access Content-Encoding allow all 
+request_header_access Content-Length allow all 
+request_header_access Content-Type allow all 
+request_header_access Date allow all 
+request_header_access Expires allow all 
+request_header_access Host allow all 
+request_header_access If-Modified-Since allow all
+request_header_access Last-Modified allow all 
+request_header_access Location allow all 
+request_header_access Pragma allow all 
+request_header_access Accept allow all 
+request_header_access Accept-Charset allow all 
+request_header_access Accept-Encoding allow all 
+request_header_access Accept-Language allow all 
+request_header_access Content-Language allow all 
+request_header_access Mime-Version allow all 
+request_header_access Retry-After allow all 
+request_header_access Title allow all 
+request_header_access Connetcion allow all 
+request_header_access Proxy-Connetcion allow all 
+request_header_access User-Agent allow all 
+request_header_access Cookie allow all 
+request_header_access All deny all
+http_access allow ncsa_auth
+EOF
+
+apt-get install apache2-utils -y
+echo "" >> /etc/squid3/squid_passwd
+touch /etc/squid3/squid_passwd
+chmod 777 /etc/squid3/squid_passwd
+htpasswd -b -c /etc/squid3/squid_user $SQUIDUSER $SQUIDPASS
+service squid3 stop
+service squid3 start
+echo "squid started on port $SQUIDPORT #"
+sleep 3;;
+
+    "8") echo "##########################"
+echo "## install teansmission ##"
+echo "##########################"
+apt-get install transmission-daemon
+mv /etc/transmission-daemon/settings.json /etc/transmission-daemon/settings.old
+
+cat > /etc/transmission-daemon/settings.json << EOF
+{
+ "alt-speed-down": 50,
+ "alt-speed-enabled": false,
+ "alt-speed-time-begin": 540,
+ "alt-speed-time-day": 127,
+ "alt-speed-time-enabled": false,
+ "alt-speed-time-end": 1020,
+ "alt-speed-up": 50,
+ "bind-address-ipv4": "$HOSTIP",
+ "bind-address-ipv6": "::",
+ "blocklist-enabled": true,
+ "blocklist-url": "http://list.iblocklist.com/?list=bt_level1&amp;fileformat=p2p&amp;archiveformat=gz",
+ "cache-size-mb": 4,
+ "dht-enabled": true,
+ "download-dir": "/home/$username/complete/",
+ "download-limit": 100,
+ "download-limit-enabled": 0,
+ "encryption": 2,
+ "idle-seeding-limit": 30,
+ "idle-seeding-limit-enabled": false,
+ "incomplete-dir": "/home/$username/ongoing/",
+ "incomplete-dir-enabled": true,
+ "lpd-enabled": false,
+ "max-peers-global": 200,
+ "message-level": 2,
+ "peer-congestion-algorithm": "",
+ "peer-limit-global": 240,
+ "peer-limit-per-torrent": 60,
+ "peer-port": $TRANPPORT,
+ "peer-port-random-high": 65535,
+ "peer-port-random-low": 49152,
+ "peer-port-random-on-start": false,
+ "peer-socket-tos": "default",
+ "pex-enabled": true,
+ "port-forwarding-enabled": true,
+ "preallocation": 1,
+ "prefetch-enabled": 1,
+ "ratio-limit": 2,
+ "ratio-limit-enabled": false,
+ "rename-partial-files": true,
+ "rpc-authentication-required": true,
+ "rpc-bind-address": "0.0.0.0",
+ "rpc-enabled": true,
+ "rpc-password": "$WEBPASS",
+ "rpc-port": "$TRANPORT",
+ "rpc-url": "/transmission/",
+ "rpc-username": "$WEBUSER",
+ "rpc-whitelist": "127.0.0.1",
+ "rpc-whitelist-enabled": false,
+ "script-torrent-done-enabled": false,
+ "script-torrent-done-filename": "",
+ "speed-limit-down": 100,
+ "speed-limit-down-enabled": false,
+ "speed-limit-up": 100,
+ "speed-limit-up-enabled": false,
+ "start-added-torrents": true,
+ "trash-original-torrent-files": false,
+ "umask": 18,
+ "upload-limit": 100,
+ "upload-limit-enabled": 0,
+ "upload-slots-per-torrent": 14,
+ "utp-enabled": true
+}
+EOF
+
+service transmission-daemon reload
+echo "transmission is now installed and running on $HOSTIP : $TRANPORT"
+sleep 3;;
+
+   "9" echo "######################"
+echo "## Installing nzedb ##"
+echo "######################"
+/etc/init.d/apparmor stop
+/etc/init.d/apparmor teardown
+update-rc.d -f apparmor remove
+apt-get install python-setuptools
+python -m easy_install
+easy_install cymysql
+apt-get install -y php5 php5-dev php-pear php5-gd php5-mysqlnd php5-curl
+cp /etc/php5/cli/php.ini /home/backup/nzedb/php.ini.back
+
+cat > /etc/php5/cli/php.ini << EOF
+
+EOF
+
+apt-get install mysql-server mysql-client libmysqlclient-dev
+sudo apt-get install apache2
+
+cat > /etc/php5/apache2/php.ini << EOF
+
+EOF
+
+cat > /etc/apache2/sites-available/nZEDb << EOF
+<VirtualHost *:80>
+     ServerName xxxx
+     ServerAdmin xxxxx
+     ServerAlias xxxx
+     DocumentRoot "/var/www/nZEDb/www"
+     ErrorLog /var/log/apache2/error.log
+     LogLevel warn
+     ServerSignature Off
+
+  <Directory "/var/www/nZEDb/www">
+         Options FollowSymLinks
+         AllowOverride All
+         Order allow,deny
+         allow from all
+  </Directory>
+
+</VirtualHost>
+EOF
+
+a2dissite default
+a2ensite nZEDb
+a2enmod rewrite
+service apache2 restart
+add-apt-repository ppa:shiki/mediainfo
+apt-get update
+apt-get install mediainfo
+apt-get install lame
+git clone https://github.com/nZEDb/nZEDb.git /var/www/
+chmod 777 /var/www/nZEDb
+cd /var/www/nZEDb
+chmod -R 755
+sudo chmod 777 /var/www/nZEDb/www/lib/smarty/templates_c
+sudo chmod -R 777 /var/www/nZEDb/www/covers
+sudo chmod 777 /var/www/nZEDb/www
+sudo chmod 777 /var/www/nZEDb/www/install
+sudo chmod -R 777 /var/www/nZEDb/nzbfiles
+echo "nZEDb is now installed goto $HOSTIP:80 to finnish off install"
+sleep 3;;
+
+    "0" echo "########################"
+echo "# installing curlftpfs #"
+echo "########################"
+sleep 1
+sudo apt-get install curlftpfs
+
+
+echo "########################"
+echo "#   add mount points   #"
+echo "########################"
+echo "curlftpfs#$FTPUSER:$FTPPASS@$FTPHOST/$FILMFTPDIR /home/media/films fuse auto,user,uid=1000,allow_other,_netdev 0 0" >> /etc/fstab
+echo "curlftpfs#$FTPUSER:$FTPPASS@$FTPHOST/$TVFTPDIR /home/media/tv fuse auto,user,uid=1000,allow_other,_netdev 0 0" >> /etc/fstab
+echo "curlftpfs#$FTPUSER:$FTPPASS@$FTPHOST/$MUSICFTPDIR /home/media/music fuse auto,user,uid=1000,allow_other,_netdev 0 0" >> /etc/fstab
+echo "curlftpfs#$FTPUSER:$FTPPASS@$FTPHOST/$BOOKFTPDIR /home/media/books fuse auto,user,uid=1000,allow_other,_netdev 0 0" >> /etc/fstab
+echo "curlftpfs#$FTPUSER:$FTPPASS@$FTPHOST/$GAMEFTPDIR /home/media/games fuse auto,user,uid=1000,allow_other,_netdev 0 0" >> /etc/fstab
+echo "curlftpfs#$FTPUSER:$FTPPASS@$FTPHOST/$COMICSFTPDIR /home/media/comics fuse auto,user,uid=1000,allow_other,_netdev 0 0" >> /etc/fstab
+echo "Curlftpfs has been installed check /etc/fstab for settings"
+sleep 3;;
+
+
+    "M")echo "######################"
+echo "# add 1GB swap space #"
+echo "######################"
+sleep 1
+sudo dd if=/dev/zero of=/swapfile bs=1024 count=1024k
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo "/swapfile       none    swap    sw      0       0" >> /etc/fstab
+echo 0 | sudo tee /proc/sys/vm/swappiness
+echo vm.swappiness = 0 | sudo tee -a /etc/sysctl.conf
+echo "Your 1gb swap space has been created"
+sleep 3;;
 
     "Q")  exit                      ;;
     "q")  echo "case sensitive!!"   ;; 
