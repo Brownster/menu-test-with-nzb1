438d8fcfb4eb7039ebe346fa47a4984a41eaebfe
Brownster <marcsmobile.brown@googlemail.com>
2014-11-04 16:04:24 +0000

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -18,7 +18,103 @@ do
 EOF
     read -n1 -s
     case "$REPLY" in
-    "1")   ;;
+    "1")  add-apt-repository ppa:jcfp/ppa
+add-apt-repository ppa:transmissionbt/ppa
+apt-get update
+HOSTIP=`ifconfig|xargs|awk '{print $7}'|sed -e 's/[a-z]*:/''/'`
+echo "i will be using: $HOSTIP as the WAN address"
+
+echo "#######################"
+echo "## create a new user ##"
+echo "#######################"
+
+echo "we will add a user so we can stop using root, please provide username and password when prompted"
+sleep2
+if [ $(id -u) -eq 0 ]; then
+	read -p "Enter username : " username
+	read -s -p "Enter password : " password
+	egrep "^$username" /etc/passwd >/dev/null
+	if [ $? -eq 0 ]; then
+		echo "$username exists!"
+		exit 1
+	else
+		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+		useradd -m -p $pass $username
+		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
+	fi
+else
+	echo "Only root may add a user to the system"
+	exit 2
+	fi
+
+#just in case we dont have git
+apt-get install git -y
+#install python now incase sabnzbsplus install fails
+apt-get install python-cheetah -y
+
+echo "####################"
+echo "## installing ufw ##"
+echo "####################"
+sleep 2
+apt-get install ufw -y
+
+
+echo "###############################"
+echo "## opening ports on firewall ##"
+echo "###############################"
+ufw allow $SSHPORT
+echo "opening old ssh port just for now to make sure we dont lose our connetcion"
+ufw allow ssh
+echo "opening new Sab web UI port"
+ufw allow $SABPORT
+echo "opening new Sickbeard web UI port"
+ufw allow $SICKPORT
+echo "opening new Couchpotato web UI port"
+ufw allow $COUCHPORT
+echo "opening new Headphones web UI port"
+ufw allow $HEADPORT
+echo "opening new Lazy Librarian web UI port"
+ufw allow $BOOKPORT
+echo "opening new Squid Proxy server Port"
+ufw allow $SQUIDPORT
+echo "opening new Transmission web UI Port"
+ufw allow $TRANPORT
+echo "opening port for Maraschino"
+ufw allow $MARAPORT
+echo "nZEDb web port"
+ufw allow 80
+echo "editing sshd config"
+sed -i "s/port 22/port $sshport/" /etc/ssh/sshd_config
+sed -i "s/protocol 3,2/protocol 2/" /etc/ssh/sshd_config
+sed -i "s/PermitRootLogin yes/PermitRootLogin no/" /etc/ssh/sshd_config
+sed -i "s/DebianBanner yes/DebianBanner no/" /etc/ssh/sshd_config
+echo "restarting ssh"
+sleep 2
+/etc/init.d/ssh restart -y
+echo "enabling firewall"
+sleep 2
+ufw enable -y
+
+
+echo "##########################"
+echo "## secure shared memory ##"
+echo "##########################"
+sleep 2
+echo "tmpfs     /run/shm     tmpfs     defaults,noexec,nosuid     0     0" >> /etc/fstab
+echo "adding admin group"
+sleep 2
+groupadd admin
+usermod -a -G admin $username
+echo "protect su by limiting access to admin group only"
+dpkg-statoverride --update --add $username admin 4750 /bin/su
+
+
+echo "############################################"
+echo "# adding $username to sudo and fuse groups #"
+echo "############################################"
+sleep 3
+usermod -a -G sudo $username
+usermod -a -G fuse $username ;;
     "2")  echo "you chose choice 2" ;;
     "3")  echo "you chose choice 3" ;;
     "Q")  exit                      ;;
