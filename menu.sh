701e7a481c44be9d96d4eb0003c9dcf1b6ec7c2d
Brownster <marcsmobile.brown@googlemail.com>
2014-11-04 22:50:15 +0000

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -172,6 +172,7 @@ do
     Install Squid Proxy 	(7)
     Install Transmission	(8)
     Install nZEDb indexer	(9)
+    Install Maraschino		(X)
     Create 1GB Swap space	(M)
     Finnished Installing close ssh port 22 and reboot 	(F)
            (Q)uit
@@ -1071,7 +1072,51 @@ sudo chmod 777 /var/www/nZEDb/www/install
 sudo chmod -R 777 /var/www/nZEDb/nzbfiles
 echo "nZEDb is now installed goto $HOSTIP:80 to finnish off install"
 sleep 3;;
+   
+    "X")echo "###########################"
+echo "## installing Maraschino ##"
+echo "###########################"
+cd /home/$username/temp
+git clone https://github.com/mrkipling/maraschino.git maraschino
+cp /home/$username/temp/maraschino /home/backups/maraschino/
+mv /home/$username/temp/maraschino  /home/$username/.maraschino
+cp /home/$username/maraschino/initd /etc/init.d/maraschino
 
+cat >/etc/default/maraschino << EOF
+# This file is sourced by /etc/init.d/maraschino
+#
+# When maraschino is started using the init script
+# is started under the account of $USER, as set below.
+#
+# Each setting is marked either "required" or "optional";
+# leaving any required setting unconfigured will cause
+# the service to not start.
+
+# [required] set path where maraschino is installed:
+APP_PATH=/home/$username/.maraschino
+
+# [optional] change to 1 to enable daemon
+ENABLE_DAEMON=1
+
+# [required] user or uid of account to run the program as:
+RUN_AS=$username
+
+# [optional] full path for the pidfile
+# otherwise, the default location /var/run/maraschino/maraschino.pid is used:
+PID_FILE=
+
+# [required] port to listen on (defaults to 7000)
+PORT=$MARAPORT
+EOF
+
+chmod a+x /etc/init.d/maraschino
+update-rc.d maraschino defaults
+chown $username /home/$username/.maraschino/
+chmod 777 /home/$username/.maraschino/
+/etc/init.d/maraschino start
+echo "Maraschino has been started on $HOSTIP : $MARAPORT";;
+    
+    
     "0") echo "########################"
 echo "# installing curlftpfs #"
 echo "########################"
