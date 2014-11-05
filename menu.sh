7164dcfe5532234ea8d60b0d4d55b431f1ebeb76
Brownster <marcsmobile.brown@googlemail.com>
2014-11-05 14:58:07 +0000

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1059,7 +1059,197 @@ album_completion_pct = 80
 cache_sizemb = 32
 journal_mode = wal
 EOF
+;;
 
+    "5") echo "###########################"
+echo "## installing Headphones ##"
+echo "###########################"
+sleep 2
+cd /home/$username/temp
+git clone https://github.com/rembo10/headphones.git  headphones
+cp /home/$username/temp/headphones /home/backups/headphones/
+mv /home/$username/temp/headphones /home/$username/.headphones
+sudo cp /home/$username/.headphones/init.ubuntu /etc/init.d/headphones
+mv /home/$username/.headphones/config.ini /home/$username/.headphones/config.orig
+touch /home/$username/.headphones/config.ini
+chown $username /home/$username/.headphones/*
+chown $username /home/$username/.headphones/*/*
+echo "will try and move config probably wont be there"
+mv /home/$username/.headphones/config.ini /home/$username/.headphones/config.old
+
+cat > /home/$username/.headphones/config.ini << EOF
+[General]
+config_version = 5
+http_port = $HEADPORT
+http_host = $HOSTIP
+http_username = $WEBUSER
+http_password = $WEBPASS
+http_root = /
+http_proxy = 0
+enable_https = 0
+https_cert = /home/$username/.headphones/server.crt
+https_key = /home/$username/.headphones/server.key
+launch_browser = 1
+api_enabled = 0
+api_key = ""
+log_dir = /home/$username/.headphones/logs
+cache_dir = /home/$username/.headphones/cache
+git_path = ""
+git_user = rembo10
+git_branch = master
+check_github = 1
+check_github_on_startup = 1
+check_github_interval = 360
+music_dir = /home/music
+destination_dir = /home/music
+lossless_destination_dir = ""
+preferred_quality = 0
+preferred_bitrate = ""
+preferred_bitrate_high_buffer = ""
+preferred_bitrate_low_buffer = ""
+preferred_bitrate_allow_lossless = 0
+detect_bitrate = 0
+auto_add_artists = 1
+correct_metadata = 1
+move_files = 1
+rename_files = 1
+folder_format = $Artist/$Album [$Year]
+file_format = $Track $Artist - $Album ($Year) - $Title
+file_underscores = 0
+cleanup_files = 1
+add_album_art = 1
+album_art_format = folder
+embed_album_art = 1
+embed_lyrics = 0
+nzb_downloader = 0
+torrent_downloader = 1
+download_dir = /home/completed/music
+blackhole_dir = ""
+usenet_retention = 1200
+include_extras = 0
+extras = ""
+autowant_upcoming = 1
+autowant_all = 0
+keep_torrent_files = 0
+numberofseeders = 10
+torrentblackhole_dir = /home/torrents
+isohunt = 0
+kat = 1
+mininova = 0
+piratebay = 1
+piratebay_proxy_url = ""
+download_torrent_dir = /home/completed/music
+search_interval = 360
+libraryscan = 1
+libraryscan_interval = 1800
+download_scan_interval = 5
+update_db_interval = 24
+mb_ignore_age = 365
+preferred_words = ""
+ignored_words = ""
+required_words = ""
+lastfm_username = ""
+interface = default
+folder_permissions = 0755
+file_permissions = 0644
+music_encoder = 0
+encoder = ffmpeg
+xldprofile = ""
+bitrate = 192
+samplingfrequency = 44100
+encoder_path = ""
+advancedencoder = ""
+encoderoutputformat = mp3
+encoderquality = 2
+encodervbrcbr = cbr
+encoderlossless = 1
+delete_lossless_files = 1
+mirror = headphones
+customhost = localhost
+customport = 5000
+customsleep = 1
+hpuser = 
+hppass = 
+[Waffles]
+waffles = 0
+waffles_uid = ""
+waffles_passkey = ""
+[Rutracker]
+rutracker = 0
+rutracker_user = ""
+rutracker_password = ""
+[What.cd]
+whatcd = 0
+whatcd_username = ""
+whatcd_password = ""
+[SABnzbd]
+sab_host = http://$HOSTIP:$SABPORT/sabnzbd
+sab_username = $WEBUSER
+sab_password = $WEBPASS
+sab_apikey = 
+sab_category = Music
+[NZBget]
+nzbget_username = nzbget
+nzbget_password = ""
+nzbget_category = ""
+nzbget_host = ""
+[Headphones]
+headphones_indexer = 1
+[Transmission]
+transmission_host = http://$HOSTIP:$TRANPORT
+transmission_username = $WEBUSER
+transmission_password = $WEBPASS
+[uTorrent]
+utorrent_host = ""
+utorrent_username = ""
+utorrent_password = ""
+[Newznab]
+newznab = 1
+newznab_host = http://
+newznab_apikey = 
+newznab_enabled = 1
+extra_newznabs = 
+[NZBsorg]
+nzbsorg = 0
+nzbsorg_uid = None
+nzbsorg_hash = ""
+[NZBsRus]
+nzbsrus = 0
+nzbsrus_uid = ""
+nzbsrus_apikey = ""
+[omgwtfnzbs]
+omgwtfnzbs = 0
+omgwtfnzbs_uid = ""
+omgwtfnzbs_apikey = ""
+[Prowl]
+prowl_enabled = 0
+prowl_keys = ""
+prowl_onsnatch = 0
+prowl_priority = 0
+[XBMC]
+xbmc_enabled = 0
+xbmc_host = ""
+xbmc_username = ""
+xbmc_password = ""
+xbmc_update = 0
+xbmc_notify = 0
+[NMA]
+nma_enabled = 0
+nma_apikey = ""
+nma_priority = 0
+nma_onsnatch = 0
+[Pushover]
+pushover_enabled = 0
+pushover_keys = ""
+pushover_onsnatch = 0
+pushover_priority = 0
+[Synoindex]
+synoindex_enabled = 0
+[Advanced]
+album_completion_pct = 80
+cache_sizemb = 32
+journal_mode = wal
+EOF
 
 cp /home/$username/.headphones/config.ini /etc/default/headphones
 chown $username /home/$username/.headphones/
@@ -1073,7 +1263,7 @@ update-rc.d headphones defaults
 echo "starting Headphones on port $HEADPORT"   
 python /home/$username/.headphones/Headphones.py --daemon
 echo "Headphones has started you can try http://$HOSTIP:$HEADPORT"
-sleep 3;;
+;;
 
 
     "6") echo "##############################"
