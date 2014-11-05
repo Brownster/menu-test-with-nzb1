e44a5e618c97c91225a2415fe80c5d8764ab1435
Brownster <marcsmobile.brown@googlemail.com>
2014-11-05 18:33:22 +0000

    Create sabnzb.sh

---
+++ sabnzb.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "########################"
+echo "## installing sabnzbd ##"
+echo "########################"
+sleep 2
+#install python now incase sabnzbsplus install fails
+apt-get install python-cheetah -y
+apt-get install sabnzbdplus -y
+mv /etc/default/sabnzbdplus /home/backups/sabnzbd/sabnzbdplus.orig
+echo "change sab config"
+
+cat > /etc/default/sabnzbdplus << EOF
+USER=$username
+CONFIG=
+HOST=$HOSTIP
+PORT=$SABPORT
+EOF
+
+chmod +x /etc/init.d/sabnzbdplus
+echo "starting sabnzbplus"
+/etc/init.d/sabnzbdplus start
+echo "sabnzbdplus is now running on $HOSTIP:$SABPORT"
